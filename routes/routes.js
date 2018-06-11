1f88843d076c8e5939595fef5ea247aadebfbdda
Leon Jalfon <leonj@sela.co.il>
2018-06-11 20:34:14 +0300

    fix response + refactoring

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,24 @@
 var appRouter = function (app) {
-var multiplication = require('./../multiplication.js');
 
 app.get("/", function(req, res) {
 	res.status(404).send("Error 404, To use the multiplication service browse to: {server}:{port}/multiplication/{number1}/{number2}");
 })
   
 app.get("/multiplication/:number1/:number2", function(req,res) {
-    var number1 = req.params.number1;
-    var number2 = req.params.number2;
-    var total = multiplication.multiplication(number1,number2);
-    res.send(JSON.stringify(total));
+    var number1 = parseInt(req.params.number1);
+    var number2 = parseInt(req.params.number2);
+    var total = number1 * number2;
+
+	var body = {
+		"first-number": number1,
+		"second-number": number2,
+		"operation": "multiplication",
+		"total": total
+	}
+	
+	res.setHeader('Access-Control-Allow-Origin', 'null');
+	return res.status(200).end(JSON.stringify(body));
+	
   });
 }
 
